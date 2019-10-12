f5412af7008a458a2949c97bf5aadfb2dc8ac948
sandr463 <stanish2000@gmail.com>
2019-10-13 02:39:04 +0300

    Implement /add_username and /best_warriors commands

--- telegram_stuff.py
+++ telegram_stuff.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import json
 from resources.credentials import main_url
+from redis_stuff.main import redis_cli
 import requests
 
 
@@ -29,22 +31,70 @@ def menu(data):
     elif message == '/add_username':
         """
             TODO:
-                - Add nickname in redis db
+                - Add CodeWars API
         """
         method = 'sendMessage'
-        params = {'chat_id': chat_id, 'text': 'Nickname was added successfully.'}
+        params = {'chat_id': chat_id, 'text': 'Enter your username:'}
         resp = requests.get(main_url + method, params)
+    elif message[0] != '/':
+        """
+            TODO:
+                - Add verification if len of list is less than 1
+        """
+        try:
+            telegram_user_id = data['message']['from']['id']
+
+            codewars_nickname = message
+
+            if len(redis_cli.lrange(telegram_user_id, 0, -1)) > 0:
+                print('exists')
+                pass
+            else:
+                redis_cli.lpush(telegram_user_id, *[codewars_nickname, 0])
+                print('pushed')
+
+                method = 'sendMessage'
+                params = {'chat_id': chat_id, 'text': 'Nickname was added successfully.'}
+                resp = requests.get(main_url + method, params)
+
+        except IndexError:
+            method = 'sendMessage'
+            params = {'chat_id': chat_id, 'text': 'Enter correct nickname.'}
+            resp = requests.get(main_url + method, params)
 
     elif message == '/team_of_the_week':
         """
             TODO:
                 - Create weekly rating
         """
-        pass
+        if datetime.today().weekday() == 0:
+            pass
 
     elif message == '/best_warriors':
         """
             TODO:
                 - Create total rating
+                Retrieve total honor directly from CodeWars !! not from redis !!
         """
-        pass
+        total_rating = {}
+
+        telegram_users_id = redis_cli.keys("*")
+        for i in telegram_users_id:
+            i = i.decode('utf-8')
+            codewars_nickname_encoded = redis_cli.lrange(i, -1, -1)[0]
+
+            codewars_nickname = codewars_nickname_encoded.decode('utf-8')
+            get_codewars_user_url = f'https://www.codewars.com/api/v1/users/{codewars_nickname}/'
+            codewars_user = requests.get(get_codewars_user_url).json()
+            total_honor = codewars_user['honor']
+
+            total_rating.update({i: {'nick': codewars_nickname, 'total_honor': total_honor}})
+
+        method = 'sendMessage'
+        text = ''
+
+        for i in total_rating:
+            text += 'username: ' + i + 'nick: ' + total_rating[i]['nick'] + 'total_honor: ' + str(total_rating[i]['total_honor']) + '\n'
+
+        params = {'chat_id': chat_id, 'text': text}
+        resp = requests.get(main_url + method, params)
