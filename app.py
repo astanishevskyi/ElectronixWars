9e1a94d85f72de2831ebdfa7e34b91013ed38274
sandr463 <stanish2000@gmail.com>
2019-10-13 04:04:13 +0300

    Add time checker to execute refresh of statistics

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from telegram_stuff import write_json, get_json, menu
-
+from weekly_stat import weekly_differ_rating
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -11,6 +12,11 @@ def hello():
         r = request.get_json()
         write_json(r)
         menu(r)
+        if datetime.today().weekday() == 6:
+            if datetime.now().hour == 1:
+                if datetime.now().minute == 1:
+                    weekly_differ_rating()
+
         return jsonify(get_json())
 
     return 'Hello World!'
